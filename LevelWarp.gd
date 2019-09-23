b3d363228c7bc5ece1307d73f663ca864efeed58
Marco Behnke <marco.behnke@innogames.com>
2019-09-23 16:22:09 +0200

    better scene linking

--- LevelWarp.gd
+++ LevelWarp.gd
@@ -1,10 +1,10 @@
 extends Area2D
 
-export var level_name: String
+export(String, FILE, "*.tscn") var next_world
 
 func _physics_process(delta):
 	var bodies = get_overlapping_bodies()
 	
 	for body in bodies:
 		if body.name == "Player":
-			get_tree().change_scene(level_name + ".tscn")
+			get_tree().change_scene(next_world)
